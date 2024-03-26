79edbfd9e5cca73576cdf70e559a3546342c8ad0
Camille Sicat <cisicat@ucsd.edu>
2024-03-26 11:58:14 -0700

    Rendered Page using HTML

--- hello-render.py
+++ hello-render.py
@@ -1,6 +1,16 @@
 from flask import render_template
+from flask import Flask
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+app = Flask(__name__)
+
+@app.route('/hello-render/')
+@app.route('/hello-render/<name>')
 def hello(name=None):
     return render_template('hello-world.html', name=name)
+
+@app.route("/") 
+def index(): 
+    return "Main Page"
+
+if __name__ == "__main__":
+   app.run()
